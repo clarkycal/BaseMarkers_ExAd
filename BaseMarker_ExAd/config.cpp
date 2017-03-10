4e4a521a0e1152a297f64d518df58b4e49c81a8a
clarky <clarkycal@gmail.com>
2017-03-10 20:54:27 +0000

    We were right the first time..
    We were right the first time..

--- BaseMarker_ExAd/config.cpp
+++ BaseMarker_ExAd/config.cpp
@@ -6,7 +6,7 @@ class CfgXM8
 	{
 		controlID = 50500;
 		title = "Base Marker";		
-		logo = "ExAdClient\XM8\Apps\BaseMarkers_ExAd\BaseMarker.paa";
-		onLoad = "ExAdClient\XM8\Apps\BaseMarkers_ExAd\onLoad.sqf";
+		logo = "ExAdClient\XM8\Apps\BaseMarker\BaseMarker.paa";
+		onLoad = "ExAdClient\XM8\Apps\BaseMarker\onLoad.sqf";
 	};
 }; 
